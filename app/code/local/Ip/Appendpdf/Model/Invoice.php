4c6bdfb3bdf366e4a2449ce9dd6f482e24055e87
ryan <ryan@inverseparadox.net>
2013-06-28 17:03:00 -0400

    Initial Commit

---
+++ app/code/local/Ip/Appendpdf/Model/Invoice.php
@@ -0,0 +1,83 @@
+<?php
+
+class Ip_Appendpdf_Model_Invoice extends Mage_Sales_Model_Order_Pdf_Invoice
+{
+
+
+    /**
+     * Insert Comments
+     *
+     * @param Zend_Pdf_Page $page
+     * @param null $store
+     */
+    protected function insertComments(&$page, $order)
+    {
+        $part = "Customer Note: ".$order->getCustomerNote();
+        $this->_setFontRegular($page, 10);
+        $page->drawText($part, 25, $this->y, 'UTF-8');
+        $this->y -= 50;
+    }
+	
+ 	/**
+     * Return PDF document
+     *
+     * @param  array $invoices
+     * @return Zend_Pdf
+     */
+    public function getPdf($invoices = array())
+    {
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
+
+        $pdf = new Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->emulate($invoice->getStoreId());
+                Mage::app()->setCurrentStore($invoice->getStoreId());
+            }
+            $page  = $this->newPage();
+            $order = $invoice->getOrder();
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
+            );
+            /* Add Comments */
+            $this->insertComments($page, $order);
+            /* Add document text and number */
+            $this->insertDocumentNumber(
+                $page,
+                Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId()
+            );
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item){
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->revert();
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }
+
+
+}
